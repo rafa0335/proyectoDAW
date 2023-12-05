faece340faf5510f51cdb4691d3ce84bec59ed19
rafa0335 <149326830+rafa0335@users.noreply.github.com>
2023-12-05 08:30:14 +0100

    actualizar carpeta

---
+++ backend-Laravel/html/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+
+        User::create([
+            'dni'=>'74063984M',
+            'name' => 'administrador',
+            'apellidos'=> 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('hola1234'),
+            'rol' => 'admin',
+        ]);
+
+
+
+
+    }
+}
